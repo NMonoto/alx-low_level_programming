1a5006827883bf1205e0084519948728a1709e88
root <root@CBSD00020.cbs.co.ls>
2022-10-20 01:24:57 +0200

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
 #include "main.h"
+/**
+ *
+ *print alphabets in lower case
+ *
+ */
 
-void print_alphabet()
+void print_alphabet(void)
 {
-    char ch;
-
-    for(ch='a'; ch<='z'; ch++)
-    {
-        _putchar(ch);
-    }
-    _putchar('\n');
-
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
 }
